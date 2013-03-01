561978a36d486f992be3362dde9e553a6793dae6
Joakim Nyman <joakim@fractalwolfe.com>
2013-03-01 22:24:01 +0200

    Added Swedish lexicon files.

---
+++ core/components/mxcalendars/lexicon/sv/default.inc.php
@@ -0,0 +1,197 @@
+<?php
+/**
+ * @package mxcalendar
+ * @subpackage lexicon-sv
+ * @author Joakim Nyman <hello@fractalwolfe.com>
+ */
+$_lang['mxcalendars'] = 'mxCalendar';
+$_lang['mxcalendars.desc'] = 'Hantera dina händelser här.';
+$_lang['mxcalendars.name'] = 'Namn';
+$_lang['mxcalendars.description'] = 'Beskrivning';
+$_lang['mxcalendars.management'] = 'mxCalendar Hanterare';
+$_lang['mxcalendars.management_desc'] = 'Hantera dina händelser och globala inställningar härifrån. Du kan redigera dem antignen genom att dubbel-klicka i rutnätet eller höger-klicka på respektive rad för ytterligare menyalternativ.';
+$_lang['mxcalendars.event_title_create'] = 'Skapa ny händelse';
+$_lang['mxcalendars.event_title_update'] = 'Uppdatera händelse';
+$_lang['mxcalendars.event_title_duplicate'] = 'Duplicera händelse';
+$_lang['mxcalendars.event_title_remove'] = 'Radera händelse';
+$_lang['mxcalendars.tab_events'] = 'Händelser';
+$_lang['mxcalendars.tab_categories'] = 'Kategorier';
+$_lang['mxcalendars.tab_settings'] = 'Globala inställningar';
+$_lang['mxcalendars.tab_calendars'] = 'Kalendrar';
+$_lang['mxcalendars.grid_col_context'] = 'Sida (kontext)';
+$_lang['mxcalendars.grid_col_title'] = 'Titel';
+$_lang['mxcalendars.grid_col_category'] = 'Kategori';
+$_lang['mxcalendars.grid_col_start'] = 'Startdatum/-tid';
+$_lang['mxcalendars.grid_col_end'] = 'Slutdatum/-tid';
+$_lang['mxcalendars.grid_col_repeate'] = 'Upprepa/Återkommande';
+$_lang['mxcalendars.grid_col_formchunk'] = 'Formulär';
+$_lang['mxcalendars.txt_search_default'] = 'Sök ...';
+$_lang['mxcalendars.btn_display_past_events'] = 'Visa tidigare händelser';
+$_lang['mxcalendars.btn_create'] = 'Skapa ny händelse';
+$_lang['mxcalendars.label_product'] = 'Produkt';
+$_lang['mxcalendars.label_industry'] = 'Industri';
+$_lang['mxcalendars.label_select_context'] = 'Välj en sida (kontext) eller blankt för alla';
+$_lang['mxcalendars.label_select_calendar'] = 'Välj en kalender';
+$_lang['mxcalendars.label_today'] = 'Idag';
+$_lang['mxcalendars.label_repeating_event'] = 'Återkommande händelse';
+$_lang['mxcalendars.label_repeat_type'] = 'Inträffar';
+$_lang['mxcalendars.label_repeaton'] = 'Inträffar den';
+$_lang['mxcalendars.label_repeat_frequency'] = 'Upprepa varje';
+$_lang['mxcalendars.label_repeat_last_occurance'] = 'Inträffade senast';
+$_lang['mxcalendars.label_description'] = 'Beskrivning';
+$_lang['mxcalendars.label_location'] = 'Plats';
+$_lang['mxcalendars.label_name'] = 'Namn';
+$_lang['mxcalendars.label_address'] = 'Adress';
+$_lang['mxcalendars.label_display_map'] = 'Visa karta';
+$_lang['mxcalendars.label_link'] = 'Länk';
+$_lang['mxcalendars.label_link_rel'] = 'Länkens Rel';
+$_lang['mxcalendars.label_link_target'] = 'Länkens Target';
+$_lang['mxcalendars.label_cancel'] = 'Avbryt';
+$_lang['mxcalendars.label_save'] = 'Spara';
+$_lang['mxcalendars.label_sunday'] = 'Söndag';
+$_lang['mxcalendars.label_monday'] = 'Måndag';
+$_lang['mxcalendars.label_tuesday'] = 'Tisdag';
+$_lang['mxcalendars.label_wednesday'] = 'Onsdag';
+$_lang['mxcalendars.label_thursday'] = 'Torsdag';
+$_lang['mxcalendars.label_friday'] = 'Fredag';
+$_lang['mxcalendars.label_saturday'] = 'Lördag';
+$_lang['mxcalendars.label_dow_7'] = 'Söndag';
+$_lang['mxcalendars.label_dow_1'] = 'Måndag';
+$_lang['mxcalendars.label_dow_2'] = 'Tisdag';
+$_lang['mxcalendars.label_dow_3'] = 'Onsdag';
+$_lang['mxcalendars.label_dow_4'] = 'Torsdag';
+$_lang['mxcalendars.label_dow_5'] = 'Fredag';
+$_lang['mxcalendars.label_dow_6'] = 'Lördag';
+
+$_lang['mxcalendars.label_daily'] = 'Dagligen';
+$_lang['mxcalendars.label_weekly'] = 'Varje vecka';
+$_lang['mxcalendars.label_monthly'] = 'Varje månad';
+$_lang['mxcalendars.label_yearly'] = 'Varje år';
+$_lang['mxcalendars.label_forms'] = 'Formulär';
+$_lang['mxcalendars.label_images'] = 'Bilder';
+$_lang['mxcalendars.label_select_image'] = 'Bild';
+$_lang['mxcalendars.btn_create_image'] = 'Lägg till bild';
+$_lang['mxcalendars.image_filepath'] = 'Filens sökväg';
+$_lang['mxcalendars.label_create'] = 'Skapa';
+$_lang['mxcalendars.menu_update'] = 'Uppdatera';
+$_lang['mxcalendars.menu_remove'] = 'Radera';
+$_lang['mxcalendars.default_search'] = 'Sök ...';
+$_lang['mxcalendars.err_ae'] = 'En kalender händelse med det namnet finns redan.';
+$_lang['mxcalendars.err_nf'] = 'Händelsens ID hittades inte i inlämnade datat.';
+$_lang['mxcalendars.err_ns'] = 'Posten hittades inte för uppdatering.';
+$_lang['mxcalendars.err_ns_name'] = 'Vänligen ange ett namn för händelsen.';
+$_lang['mxcalendars.err_ns_title'] = 'Vänligen ange en titel för händelsen.';
+$_lang['mxcalendars.err_remove'] = 'Ett fel uppstod när händelsen skulle raderas.';
+$_lang['mxcalendars.err_save'] = 'Ett fel uppstod när händelsen skulle sparas.';
+$_lang['mxcalendars.err_event_req_name'] = 'Namn är obligatoriskt';
+$_lang['mxcalendars.err_event_req_category'] = 'Kategori är obligatoriskt';
+$_lang['mxcalendars.err_event_req_startdate'] = 'Startdatum är obligatoriskt';
+$_lang['mxcalendars.err_event_req_starttime'] = 'Starttid är obligatoriskt';
+$_lang['mxcalendars.err_event_req_enddate'] = 'Slutdatum är obligatoriskt';
+$_lang['mxcalendars.err_event_req_endtime'] = 'Sluttid är obligatoriskt';
+$_lang['mxcalendars.err_event_req_context'] = 'Kontext är obligatoriskt';
+$_lang['mxcalendars.label_select_context_err'] = 'Alla sidor';
+$_lang['mxcalendars.err_event_req_repeattype'] = 'Upprepningstyp är obligatoriskt för återkommande händelser';
+$_lang['mxcalendars.err_event_req_repeatfrequency'] = 'Upprepa varje (frekvensen), är obligatoriskt för återkommande händelser';
+$_lang['mxcalendars.err_event_req_repeatenddate'] = 'Upprepningens slutdatum är obligatoriskt för återkommande händelser';
+$_lang['mxcalendars.err_event_req_repeaton'] = 'Att ange när händelsen inträffar är är obligatoriskt för veckovis upprepande händelser';
+$_lang['mxcalendars.err_event_req_validcat'] = 'Ingen giltig kategori id hittades för standard eller publicerad, vänligen kontrollera att du har en giltig kategori skapad.';
+$_lang['mxcalendars.notice_no_events'] = 'Inga händelser hittades';
+
+$_lang['mxcalendars.mxcalendars_remove_confirm'] = 'Är du säker att du vill radera denna händelse?';
+$_lang['mxcalendars.downloads'] = 'Nedladdningar';
+$_lang['mxcalendars.location'] = 'Plats';
+$_lang['mxcalendars.search_default_text'] = 'Sök händelser...';
+$_lang['mxcalendars.top_downloaded'] = 'Mest nedladdade händelser';
+$_lang['mxcalendars.categoryid_col_label'] = 'Kategori';
+$_lang['mxcalendars.startdate_col_label'] = 'Startdatum';
+$_lang['mxcalendars.starttime_col_label'] = 'Starttid';
+$_lang['mxcalendars.enddate_col_label'] = 'Slutdatum';
+$_lang['mxcalendars.endtime_col_label'] = 'Sluttid';
+$_lang['mxcalendars.repeating_col_label'] = 'Återkommande';
+$_lang['mxcalendars.repeating_last_occ_col_label'] = 'Senaste inträffande';
+
+$_lang['mxcalendars.label_startdate'] = 'Startdatum/-tid';
+$_lang['mxcalendars.label_starttime'] = 'Starttid';
+$_lang['mxcalendars.label_title'] = 'Namn / Titel';
+$_lang['mxcalendars.label_enddate'] = 'Slutdatum/-tid';
+$_lang['mxcalendars.label_duplicate'] = 'Duplicera';
+$_lang['mxcalendars.label_select_category'] = 'Välj en kategori';
+$_lang['mxcalendars.label_select_category_err'] = 'Välj en giltig categori';
+$_lang['mxcalendars.label_calendar_id'] = 'Kalender ID';
+
+$_lang['mxcalendars.label_select_form'] = 'Välj ett formulär';
+$_lang['mxcalendars.label_select_form_err'] = 'Välj en giltig formulär chunk';
+
+$_lang['mxcalendars.btn_show_past_events'] = 'Visa tidigare händelser';
+$_lang['mxcalendars.btn_show_upcoming_events'] = 'Visa uppkommande händelser';
+$_lang['mxcalendars.tip_repeaton'] = 'Upprepa varje ___ (dagar|veckor|månader|år).<br /><br />Sätt upprepningsfrekvensen för typen (dagligen, varje vecka, varje månad, varje år) vald ovan';
+//$_lang['mxcalendars.'] = '';
+
+//-- Category Management
+$_lang['mxcalendars.category_desc'] = 'Hantera kategorierna som varje händelse kan tilldelas. Du kan också skapa anpassade färger för texten och bakgrunden för varje kategori.';
+$_lang['mxcalendars.label_window_create'] = 'Skapa ny kategori';
+$_lang['mxcalendars.category_name_col_label'] = 'Namn';
+$_lang['mxcalendars.category_isdefault_col_label'] = 'Är standard';
+$_lang['mxcalendars.category_foregroundcss_col_label'] = 'Textfärg';
+$_lang['mxcalendars.category_backgroundcss_col_label'] = 'Bakgrundsfärg';
+$_lang['mxcalendars.category_inlinecss_col_label'] = 'Infogad CSS';
+$_lang['mxcalendars.category_disabled_col_label'] = 'Inaktiv';
+$_lang['mxcalendars.category_active_col_label'] = 'Aktiv';
+$_lang['mxcalendars.cateogry_remove_title'] = 'Radera kategori';
+$_lang['mxcalendars.cateogry_remove_confirm'] = 'Är du säker att du vill radera denna kategori?';
+$_lang['mxcalendars.btn_create_cat'] = 'Skapa ny kategori';
+$_lang['mxcalendars.default_category_search'] = "Sök kategorier...";
+$_lang['mxcalendars.menu_update_category'] = 'Uppdatera kategori';
+$_lang['mxcalendars.menu_remove_category'] = 'Radera kategori';
+
+$_lang['mxcalendars.label_category_heading'] = 'Kategorier:';
+$_lang['mxcalendars.label_category_viewAll'] = 'Visa alla';
+
+//-- Settings friendly names
+$_lang['mxcalendars.set_category_required'] = 'Kategori krävs för händelse';
+$_lang['mxcalendars.set_event_desc_type'] = 'Beskrivningsfältets typ';
+$_lang['mxcalendars.set_mgr_dateformat'] = 'Datumformat';
+$_lang['mxcalendars.set_mgr_timeformat'] = 'Tidsformat';
+
+//-- Calendar Management
+$_lang['mxcalendars.calendar_desc'] = 'Hantera kalendrar som händelser kan tilldelas till, skapa så många du behöver.';
+
+$_lang['mxcalendars.err_ac'] = 'Fel uppstod vid sparandet av kalendern, vänligen försök igen.';
+$_lang['mxcalendars.calendar_name_col_label'] = 'Namn';
+$_lang['mxcalendars.calendar_name_col_active'] = 'Aktiv';
+$_lang['mxcalendars.calendar_default_calendar_search'] = 'Sök ...';
+$_lang['mxcalendars.calendar_btn_create'] = 'Skapa ny kalender';
+
+
+$_lang['mxcalendars.calendar_context_menu_update'] = 'Uppdatera';
+$_lang['mxcalendars.calendar_context_menu_remove'] = 'Radera';
+
+$_lang['mxcalendars.label_select_calendar'] = 'Välj kalender';
+$_lang['mxcalendars.label_select_calendar_err'] = 'Kalendern hittades inte';
+$_lang['mxcalendars.grid_col_calendar'] = 'Kalender';
+
+//-- Feed lexicon entries
+$_lang['mxcalendars.tab_feed'] = 'Flöden'; 
+$_lang['mxcalendars.feed_desc'] = 'Ange en extern hemsidas iCalendar eller XML flödes url som är publikt åtkomlig genom din server.'; 
+
+$_lang['mxcalendars.default_feed_search'] = 'Sök flöden';
+$_lang['mxcalendars.feed_btn_create'] = 'Skapa nytt flöde';
+$_lang['mxcalendars.feed_menu_update'] = 'Uppdatera flöde';
+$_lang['mxcalendars.feed_menu_remove'] = 'Radera flöde';
+$_lang['mxcalendars.feed_remove_title'] = 'Bekräfta radering av flöde';
+$_lang['mxcalendars.feed_remove_confirm'] = 'Detta kommer att radera flödet fullständigt ur databasen och inga ytterligare import handlingar kommer att utföras.';
+$_lang['mxcalendars.feed_col_feed'] = 'Flödets URL';
+$_lang['mxcalendars.feed_col_active'] = 'Aktiv';
+$_lang['mxcalendars.feed_col_type'] = 'Typ';
+$_lang['mxcalendars.lastrunon_col_label'] = 'Senaste uppdatering';
+$_lang['mxcalendars.nextrunon_col_label'] = 'Nästa uppdatering';
+$_lang['mxcalendars.label_default'] = 'Standard';
+$_lang['mxcalendars.source'] = 'Källa';
+$_lang['mxcalendars.label_feedmeasurementtime'] = 'Ange antal enheter';
+$_lang['mxcalendars.label_feedmeasurementtype'] = 'Timer typ';
+
+$_lang['mxcalendars.feed_type_label_select_empty'] = '--välj--';
+$_lang['mxcalendars.measurement_type_label_select_empty'] = '--välj--';
+
+?>
---
+++ core/components/mxcalendars/lexicon/sv/properties.inc.php
@@ -0,0 +1,41 @@
+<?php
+$_lang['prop_mxcalendars.title_desc'] = 'Kommaseparerad lista med poster att använda som titlar i bokstavsordning';
+$_lang['prop_mxcalendars.ascending'] = 'Stigande';
+$_lang['prop_mxcalendars.descending'] = 'Fallande';
+$_lang['prop_mxcalendars.dir_desc'] = 'Ordning att sortera enligt.';
+$_lang['prop_mxcalendars.sort_desc'] = 'Fält att sortera enligt.';
+
+$_lang['prop_mxcalendars.desc_theme'] = '';
+//desc_detailid
+$_lang['prop_mxcalendars.desc_displaytype'] = 'Detta anger standard vyn att använda om displayType parametern inte är angiven.';
+$_lang['prop_mxcalendars.lt_calendar'] = 'calendar';
+$_lang['prop_mxcalendars.lt_list'] = 'list';
+$_lang['prop_mxcalendars.lt_mini'] = 'mini';
+$_lang['prop_mxcalendars.desc_elstartdate'] = 'Startdatum för händelselista';
+$_lang['prop_mxcalendars.desc_elenddate'] = 'Slutdatum för händelselista';
+$_lang['prop_mxcalendars.desc_tpllistitem'] = 'Den innersta chunken för händelsen i en händelselista';
+$_lang['prop_mxcalendars.desc_tpllistheading'] = 'Standard rubrik att placera ovanför listan';
+$_lang['prop_mxcalendars.desc_tpllistwrap'] = 'Den yttersta chunken som omsluter händelselistan';
+$_lang['prop_mxcalendars.desc_tpldetail'] = 'Den chunk som används för händelsens detaljvy';
+$_lang['prop_mxcalendars.desc_dateformat'] = 'Standard datumformat för formaterade datum, använder serverskript för att skriva ut formaterat datum.';
+$_lang['prop_mxcalendars.desc_timeformat'] = 'Standard tidsformat för formaterade tider, använder serverskript för att skriva ut formaterad tid';
+$_lang['prop_mxcalendars.desc_dateseperator'] = 'Ange en standard avskiljare för datumspjälkning. "-" är standard';
+$_lang['prop_mxcalendars.desc_activemonthonlyevents'] = 'Sätt till true för att endast visa händelser för aktiv månad';
+$_lang['prop_mxcalendars.desc_highlighttoday'] = 'Boolesk egenskap (true/false) för att ändra klass på dagens datum i kalendervyn';
+$_lang['prop_mxcalendars.desc_todayclass'] = 'Ange klassnamn för att markera dagens datum i kalendervyn';
+$_lang['prop_mxcalendars.desc_noeventclass'] = 'Ange en specialklass för datum some saknar händelser';
+$_lang['prop_mxcalendars.desc_haseventsclass'] = 'Ange en specialklass för datum som har händelser';
+$_lang['prop_mxcalendars.desc_tplevent'] = '';
+$_lang['prop_mxcalendars.desc_tplday'] = '';
+$_lang['prop_mxcalendars.desc_tplweek'] = '';
+$_lang['prop_mxcalendars.desc_tplmonth'] = '';
+$_lang['prop_mxcalendars.desc_tplheading'] = '';
+$_lang['prop_mxcalendars.desc_debug'] = '';
+$_lang['prop_mxcalendars.desc_googleMapAPIKey'] = 'Ange din personliga Google Map V3 API nyckel för spårning och att sträcka dig utanför gränserna för anonyma api förfrågningar';
+$_lang['prop_mxcalendars.desc_googleMapRegion'] = 'Tillåter dig att ange regionförspänning för Google Map API baserat på CcTLD (http://en.wikipedia.org/wiki/CcTLD)';
+
+$_lang['prop_mxcalendars.tpl'] = '';
+
+
+
+$_lang['prop_mxcalendars.listing_type'] = 'Standard listtyp för resultat';
